629543b5f2bee9c9e74b815d260affc1d15c0b67
Denis Chuprunov <denis@chuprunov.name>
2020-02-28 19:58:19 +0300

    Ashot Kantaria plugin

---
+++ plugins/ashot.py
@@ -0,0 +1,17 @@
+from kutana import Plugin
+import psutil
+import time
+import os
+
+
+plugin = Plugin(name="Ashot", description="Ashot Kantaria")
+
+
+@plugin.on_commands(["a"])
+async def _(msg, ctx):
+    process = psutil.Process(os.getpid())
+
+//    taken_memory = int(process.memory_info().rss / 2**20)
+//    taken_time = time.time() - msg.date
+
+    await ctx.reply("Ашот Кантария был очень богатым человеком")
