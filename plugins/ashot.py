a0890c5a4332356a155c4a28403e55af604dafab
Denis Chuprunov <denis@chuprunov.name>
2020-02-28 20:05:12 +0300

    Fix comment

--- plugins/ashot.py
+++ plugins/ashot.py
@@ -11,7 +11,7 @@ plugin = Plugin(name="Ashot", description="Ashot Kantaria")
 async def _(msg, ctx):
     process = psutil.Process(os.getpid())
 
-//    taken_memory = int(process.memory_info().rss / 2**20)
-//    taken_time = time.time() - msg.date
+#    taken_memory = int(process.memory_info().rss / 2**20)
+#    taken_time = time.time() - msg.date
 
     await ctx.reply("Ашот Кантария был очень богатым человеком")
