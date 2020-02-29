bc50df33f47bbd198914be9b2bccfbd2208bf6c0
Denis Chuprunov <denis@chuprunov.name>
2020-03-01 00:44:12 +0300

    find and send music file

--- plugins/music.py
+++ plugins/music.py
@@ -1,12 +1,27 @@
-from kutana import Plugin, Attachment, get_path
-
+from kutana import Plugin, Attachment, HandlerResponse, get_path
+import os
+import re
 
 plugin = Plugin(name="Music", description="Send music")
 
 
 @plugin.on_commands(["m"])
 async def _(msg, ctx):
-    with open(get_path(__file__, "assets/09. car_de_luxe.mp3"), "rb") as fh:
-        audio_message = Attachment.new(fh.read(), "car_de_luxe.mp3", "voice")
+    files = files_find("/home/hord/Музыка/", ctx.body)
+    
+    if (len(files) == 0):
+        await ctx.reply("Нет такой музыки...")
+        
+    else:
+        filepath = files.pop()
+        
+        with open(get_path(__file__, filepath), "rb") as fh:
+            audio_message = Attachment.new(fh.read(), os.path.basename(filepath), "voice")
+    
+        await ctx.reply(os.path.basename(filepath), attachments=audio_message)
 
-    await ctx.reply("Audio message", attachments=audio_message)
+def files_find(catalog, f):
+    find_files = []
+    for root, dirs, files in os.walk(catalog):
+        find_files += [os.path.join(root, name) for name in files if re.search(f, name, re.IGNORECASE)]
+    return find_files
